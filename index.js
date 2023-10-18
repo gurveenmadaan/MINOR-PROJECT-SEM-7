a66fff60d677213dbca6f1dfd4c08c4244466eb7
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-18 13:07:58 +0530

    tested the basic backend-check working

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ const db =require('./config/mongoose');
 //used for session cookie and authentication passport
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require('./config/passport-local-stratergy');
-const MongoStore = require('connect-mongo')(session);
+// const passportLocal = require('./config/passport-local-stratergy');
+const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
 
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest: './assets/css',
+    debug: true , 
+    outputStyle: 'extended',
+    prefix:'/css'
+}));
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static('./assets'));
@@ -24,29 +32,33 @@ app.set('layout extractScripts',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
-app.use(session({
-    name:'Minor-Project',
 
-    //changeb the secret before deployment
-    secret: 'blahsomething',
 
-    saveUninitialized:false,
-    resave:false,
-    cookie:{
-        maxAge:(1000*60*100)
-    },
-    store:new MongoStore({
-            mongooseConnection: db,
-            autoRemove:'disabled'
-    }, function(err){
+
+app.use(
+    session({
+      name: 'codeial',
+      secret: 'blahsomething',
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 1000 * 60 * 100,
+      },
+      store: new MongoStore({
+        mongoUrl: 'mongodb://127.0.0.1:27017/minor-project', // Replace with your MongoDB URL
+        mongooseConnection: db,
+        autoRemove: 'disabled'
+      }, function (err) {
         console.log(err || 'connect-mongodb setup ok');
+      })
     })
+  );
+  
 
-}));
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(passport.setAuthenticatedUser);
+// app.use(passport.setAuthenticatedUser);
 // use express router 
 app.use('/', require('./routes')); 
 
